19ee44c34f216bf984c92d95811d6411c8b2eb26
Lucas M Nunes <105028691+lucasdevnunes@users.noreply.github.com>
2024-09-28 16:41:29 -0300

    Implementaca do metodo que informa se um numero pertence a sequencia Fibonacci.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import testeTecnicoTarget.exercicio1.Ficonacci;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Ficonacci fibonacci = new Ficonacci();
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual número gastaria de saber se pertence à sequência Fibonacci? ");
+        int numero = scanner.nextInt();
+        fibonacci.informarPertencimentoNoFibonacci(numero);
     }
 }
--- src/testeTecnicoTarget/exercicio1/Ficonacci.java
+++ src/testeTecnicoTarget/exercicio1/Ficonacci.java
@@ -8,13 +8,13 @@ public class Ficonacci {
     private List listaFibonacci;
 
     public Ficonacci() {
-        List<Long> fibonacci = new ArrayList<>();
-        fibonacci.add(0L);
-        fibonacci.add(1L);
-        for (int i = 1; i < 90; i++) {
-            long numeroAtual = fibonacci.get(i);
-            long numeroAnterior = fibonacci.get(i - 1);
-            long numeroPosterior = numeroAnterior + numeroAtual;
+        List<Integer> fibonacci = new ArrayList<>();
+        fibonacci.add(0);
+        fibonacci.add(1);
+        for (int i = 1; i < 45; i++) {
+            int numeroAtual = fibonacci.get(i);
+            int numeroAnterior = fibonacci.get(i - 1);
+            int numeroPosterior = numeroAnterior + numeroAtual;
             fibonacci.add(numeroPosterior);
         }
         this.listaFibonacci = fibonacci;
@@ -24,4 +24,11 @@ public class Ficonacci {
         return this.listaFibonacci;
     }
 
+    public void informarPertencimentoNoFibonacci(int numero){
+        if (this.listaFibonacci.contains(numero)){
+            System.out.println("O número " + numero + " pertence à sequência Fibonacci");
+        } else {
+            System.out.println("O número " + numero + " não pertence à sequêcia Fibonacci.");
+        }
+    }
 }
